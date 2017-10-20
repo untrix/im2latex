Fixed bug in softmax in the reset code.

src/CALSTM.py
@@ -118,7 +118,7 @@ class CALSTM(tf.nn.rnn_cell.RNNCell):
                     L = CONF.L
                     D = CONF.D
                     h = h_prev
-                    # n = self.output_size
+                    n = self.output_size
 
                     self.assertOutputShape(h_prev)
                     assert K.int_shape(a) == (B, L, D)
@@ -133,7 +133,8 @@ class CALSTM(tf.nn.rnn_cell.RNNCell):
                         h = tf.identity(K.tile(K.expand_dims(h, axis=1), (1,L,1)), name='h_t-1')
                         a = tf.identity(a, name='a')
                         ## Concatenate a and h. Final shape = (B, L, D+n)
-                        ah = tf.concat([a,h], -1, name='a_concat_h'); # (B, L, D+n)
+                        ah = tf.concat([a,h], -1, name='ai_h'); # (B, L, D+n)
+                        assert K.int_shape(ah) == (B, L, D+n)
                         if CONF.att_model == 'MLP_shared':
                             ## For #layers > 1 this implementation will endup being different than the paper's implementation because they only 
                             ## Below is how it is implemented in the code released by the authors of the paper
@@ -147,9 +148,10 @@ class CALSTM(tf.nn.rnn_cell.RNNCell):
                             ##     ah = tanh(ah)
                             ##     alpha = Dense(softmax_layer_params, activation=softmax)(ah)
 
-                            ah = tfc.MLPStack(CONF.att_layers)(ah) # (B, L, dimctx)
-                            assert K.int_shape(ah)[-1] == 1 ## (B, L, 1)
-                            ah = K.squeeze(ah, axis=2) # output shape = (B, L)
+                            alpha_1_ = tfc.MLPStack(CONF.att_layers)(ah) # (B, L, dimctx)
+                            assert K.int_shape(alpha_1_) == (B,L,1) ## (B, L, 1)
+                            alpha_ = K.squeeze(alpha_1_, axis=2) # output shape = (B, L)
+                            assert K.int_shape(alpha_) == (B,L)
 
                         elif CONF.att_model == '1x1_conv':
                             """
@@ -159,9 +161,10 @@ class CALSTM(tf.nn.rnn_cell.RNNCell):
                             Using a convnet layer of this type may actually be more efficient (and easier to code).
                             """
                             ah = tf.expand_dims(ah, axis=1)
-                            ah = tfc.ConvStack(CONF.att_layers, (B, 1, L, D+self.output_size))(ah)
-                            assert K.int_shape(ah) == (B,1,L,1)
-                            ah = tf.squeeze(ah, axis=[1,3]) # (B, L)
+                            alpha_1_ = tfc.ConvStack(CONF.att_layers, (B, 1, L, D+self.output_size))(ah)
+                            assert K.int_shape(alpha_1_) == (B,1,L,1)
+                            alpha_ = tf.squeeze(alpha_1_, axis=[1,3]) # (B, L)
+                            assert K.int_shape(alpha_) == (B,L)
                     
                     elif CONF.att_model == 'MLP_full': # MLP: weights not shared across L
                         ## concatenate a and h_prev and pass them through a MLP. This is different than the theano
@@ -173,14 +176,17 @@ class CALSTM(tf.nn.rnn_cell.RNNCell):
                         with tf.variable_scope('a_h'):
                             a_ = K.batch_flatten(a) # (B, L*D)
                             a_.set_shape((B, L*D)) # Flatten loses shape info
-                            ah = K.concatenate([a_, h]) # (B, L*D+n)
+                            ah = tf.concat([a_, h], -1, name="a_h") # (B, L*D + n)
                             assert K.int_shape(ah) == (B, L*D + self.output_size), 'shape %s != %s'%(K.int_shape(ah),(B, L*D + self.output_size))
-                        ah = tfc.MLPStack(CONF.att_layers)(ah) # (B, L)
-                        assert K.int_shape(ah) == (B, L)
+                        alpha_ = tfc.MLPStack(CONF.att_layers)(ah) # (B, L)
+                        assert K.int_shape(alpha_) == (B, L)
+
                     else:
                         raise AttributeError('Invalid value of att_model param: %s'%CONF.att_model)
 
-                    alpha = tf.identity(ah, name='alpha')
+                    ## Softmax
+                    alpha = tf.identity(tf.nn.softmax(alpha_), name='alpha')
+                    assert K.int_shape(alpha) == (B, L)
 
                     ## Attention Modulator
                     if CONF.att_modulator is not None:

src/hyper_params.py
@@ -37,21 +37,6 @@ from dl_commons import (PD, instanceof, integer, integerOrNone, decimal, boolean
 from tf_commons import (ConvStackParams, ConvLayerParams, MaxpoolParams, FCLayerParams, MLPParams, 
                         DropoutParams, TensorboardParams, RNNParams)
 
-def setLogLevel(logger, level):
-    logging_levels = (logging.CRITICAL, logging.ERROR, logging.WARNING, logging.INFO, logging.DEBUG)
-    logger.setLevel(logging_levels[level - 1])
-
-def makeFormatter():
-    return logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-
-def makeLogger(logging_level=3, name='default'):
-    logger = logging.Logger(name)
-    ch = logging.StreamHandler()
-    ch.setFormatter(makeFormatter())
-    logger.addHandler(ch)
-    setLogLevel(logger, logging_level)
-    return logger
-
 def pad_image_shape(shape, padding):
     return (shape[0] + 2*padding, shape[1] + 2*padding) + shape[2:]
 
@@ -278,11 +263,11 @@ class CALSTMParams(dlc.HyperParams):
                 'layers': (
                     ConvLayerParams(self).updated({'output_channels':self.D, 'kernel_shape':(1,1), 'stride':(1,1), 'padding':'VALID', 'activation_fn': tf.nn.relu}).freeze(),
                     ConvLayerParams(self).updated({'output_channels':self.D, 'kernel_shape':(1,1), 'stride':(1,1), 'padding':'VALID', 'activation_fn': tf.nn.relu}).freeze(),
-                    ConvLayerParams(self).updated({'output_channels':1,      'kernel_shape':(1,1), 'stride':(1,1), 'padding':'VALID', 'activation_fn': tf.nn.softmax}).freeze()
+                    ConvLayerParams(self).updated({'output_channels':1,      'kernel_shape':(1,1), 'stride':(1,1), 'padding':'VALID', 'activation_fn': None}).freeze()
                     )
                 }).freeze()
             assert self.att_layers.layers[-1].output_channels == 1, 'num output_channels of the final layer of the att_convnet should equal 1'
-            assert self.att_layers.layers[-1].activation_fn == tf.nn.softmax
+            assert self.att_layers.layers[-1].activation_fn == None ## Softmax activation will be added after squeezing dims
 
         elif self.att_model == 'MLP_shared':
             self.att_layers = MLPParams(self).updated({
@@ -291,12 +276,12 @@ class CALSTMParams(dlc.HyperParams):
                 'layers': (
                     FCLayerParams(self).updated({'num_units': self.D, 'activation_fn': tf.nn.tanh}).freeze(),
                     FCLayerParams(self).updated({'num_units': self.D, 'activation_fn': tf.nn.tanh}).freeze(),
-                    FCLayerParams(self).updated({'num_units': 1, 'activation_fn': tf.nn.softmax, 'dropout': None}).freeze(),
+                    FCLayerParams(self).updated({'num_units': 1,      'activation_fn': None, 'dropout': None}).freeze(),
                     )
                 }).freeze()
             assert self.att_layers.layers[-1].num_units == 1, 'num_units of the final layer of the att_kernel should equal 1'
-            assert self.att_layers.layers[-1].activation_fn == tf.nn.softmax
-            assert self.att_layers.layers[-1].dropout == None
+            assert self.att_layers.layers[-1].activation_fn == None ## Softmax activation will be added after squeezing dims
+            assert self.att_layers.layers[-1].dropout == None ## No droput before softmax activation
 
         elif self.att_model == 'MLP_full':
             self.att_layers = MLPParams(self).updated({
@@ -304,12 +289,12 @@ class CALSTMParams(dlc.HyperParams):
                 'layers': (
                     FCLayerParams(self).updated({'num_units': self.L, 'activation_fn': tf.nn.tanh}).freeze(),
                     FCLayerParams(self).updated({'num_units': self.L, 'activation_fn': tf.nn.tanh}).freeze(),
-                    FCLayerParams(self).updated({'num_units': self.L, 'activation_fn': tf.nn.softmax, 'dropout': None}).freeze(),
+                    FCLayerParams(self).updated({'num_units': self.L, 'activation_fn': None, 'dropout': tf.nn.softmax}).freeze(),
                     )
                 }).freeze()
             assert self.att_layers.layers[-1].num_units == self.L, 'num_units of the final layer of the att_MLP should equal L(%d)'%self.L
-            assert self.att_layers.layers[-1].activation_fn == tf.nn.softmax
-            assert self.att_layers.layers[-1].dropout == None
+            assert self.att_layers.layers[-1].activation_fn == None ## softmax activation will be added in code for consistency
+            assert self.att_layers.layers[-1].dropout == None ## No droput before/after softmax activation
 
         #### Attention Modulator ####
         self.att_modulator = MLPParams(self).updated({
@@ -541,7 +526,7 @@ class Im2LatexModelParams(dlc.HyperParams):
                 'layers': (
                     ## paper has activation set to relu for all but the softmax layer
                     ## paper has all hidden layers with num_units = m. I've noticed that they build rectangular MLPs, i.e. not triangular.
-                    # FCLayerParams(self).updated({'num_units': self.m, 'activation_fn':tf.nn.relu}).freeze(),
+                    FCLayerParams(self).updated({'num_units': self.m, 'activation_fn':tf.nn.relu}).freeze(),
                     ## Last layer must have num_units = K and activation_fn=None because it outputs logits.
                     FCLayerParams(self).updated({'num_units': self.K, 'activation_fn':None, 'dropout': None}).freeze(),
                     )
@@ -554,7 +539,7 @@ class Im2LatexModelParams(dlc.HyperParams):
                 'layers': (
                     ## paper has activation set to relu for all but the softmax layer
                     ## paper has all hidden layers with num_units = m. I've noticed that they build rectangular MLPs, i.e. not triangular.
-                    # FCLayerParams(self).updated({'num_units': self.m, 'activation_fn':tf.nn.relu}).freeze(),
+                    FCLayerParams(self).updated({'num_units': self.m, 'activation_fn':tf.nn.relu}).freeze(),
                     FCLayerParams(self).updated({'num_units': self.m, 'activation_fn':tf.nn.relu}).freeze(),
                     ## Last layer must have num_units = K and activation_fn=None because it outputs logits.
                     FCLayerParams(self).updated({'num_units': self.K, 'activation_fn':None, 'dropout': None}).freeze(),
@@ -566,9 +551,8 @@ class Im2LatexModelParams(dlc.HyperParams):
             assert len(self.output_layers.layers) >= 2, "Need one hidden layer at least to match the paper's complexity."
 
         ######## Init Model ########
-        if False: ## No hidden init layers by default
+        if True: ## No hidden init layers by default
             self.init_model_hidden = MLPParams(self).updated({
-                # 'activation_fn': tf.nn.relu,
                 'layers': (
                     ## paper sets hidden activations=relu and final=tanh
                     ## The paper's source sets all hidden units to D

src/tf_commons.py
@@ -244,7 +244,7 @@ class FCLayer(object):
 
     def __call__(self, inp, layer_idx=None):
         with tf.variable_scope(self.my_scope) as var_scope:
-            with tf.name_scope(var_scope.original_name_scope):
+            with tf.name_scope(var_scope.original_name_scope) as name_scope:
                 ## Parameter Validation
                 assert isinstance(inp, tf.Tensor)
                 if self._batch_input_shape is not None:
@@ -254,6 +254,9 @@ class FCLayer(object):
                 prefix = 'FC' if params.activation_fn is not None else 'Affine'
                 scope_name = prefix + '_%d'%(layer_idx+1) if layer_idx is not None else prefix
                 with tf.variable_scope(scope_name) as var_scope:
+                    if len(K.int_shape(inp)) != 2:
+                        logger.warn('Input to operation %s/%s has rank !=2 (%d)', name_scope, scope_name, len(K.int_shape(inp)))
+
                     coll_w = scope_name + '/_weights_'
                     coll_b = scope_name + '/_biases_'
                     var_colls = {'biases':[coll_b], 'weights':[coll_w, "REGULARIZED_WEIGHTS"]}
@@ -568,7 +571,7 @@ class DropoutLayer(object):
 
     def __call__(self, inp, layer_idx=None):
         with tf.variable_scope(self.my_scope) as var_scope:
-            with tf.name_scope(var_scope.original_name_scope):
+            with tf.name_scope(var_scope.original_name_scope) as name_scope:
                 ## Parameter Validation
                 assert isinstance(inp, tf.Tensor)
                 if self._batch_input_shape is not None:
@@ -577,6 +580,8 @@ class DropoutLayer(object):
                 params = self._params
                 scope_name = 'Dropout_%d'%(layer_idx+1) if layer_idx is not None else 'Dropout'
                 with tf.variable_scope(scope_name):
+                    if len(K.int_shape(inp)) != 2:
+                        logger.warn('Input to operation %s/%s has rank !=2 (%d)', name_scope, scope_name, len(K.int_shape(inp)))
                     return tf.nn.dropout(inp, params.keep_prob, seed=params.seed)
 
 class ActivationParams(HyperParams):
@@ -609,15 +614,18 @@ class Activation(object):
 
     def __call__(self, inp, layer_idx=None):
         with tf.variable_scope(self.my_scope) as var_scope:
-            with tf.name_scope(var_scope.original_name_scope):
+            with tf.name_scope(var_scope.original_name_scope) as name_scope:
                 ## Parameter Validation
                 assert isinstance(inp, tf.Tensor)
                 if self._batch_input_shape is not None:
                     assert K.int_shape(inp) == self._batch_input_shape
 
                 params = self._params
+
                 scope_name = 'Activation_%d'%(layer_idx+1) if layer_idx is not None else 'Activation'
                 with tf.variable_scope(scope_name):
+                    if len(K.int_shape(inp)) != 2:
+                        logger.warn('Input to operation %s/%s has rank !=2 (%d)', name_scope, scope_name, len(K.int_shape(inp)))
                     a = params.activation_fn(inp)
 
                 if ('dropout' in params) and (params.dropout is not None):